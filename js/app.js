456cd80b3d395c99a316afb9de20589a469edf20
Shaikhah Maasher <sh.maasher@gmail.com>
2018-11-19 08:56:50 +0300

    text animations

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
 
-function getWindowHeight() {
-    return screen.height;
-}
+// function getWindowHeight() {
+//     return screen.height;
+// }
 
-function setHeroHeight() {
-    var hero = document.querySelector('#hero');
-    hero.style.height = getWindowHeight;
-}
+// function setHeroHeight() {
+//     var hero = document.querySelector('#hero');
+//     hero.style.height = getWindowHeight;
+// }
 
-setHeroHeight();
+// setHeroHeight();
+
+// Initialize WOW library
+anime = new WOW (
+    {
+        boxClass:     'anime',     
+        animateClass: 'animated', 
+        offset:       10,          
+        mobile:       true,       
+        live:         true
+    }
+);
+anime.init();
+// new WOW().init();
