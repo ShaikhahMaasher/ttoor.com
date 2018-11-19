5e6994bfb1fc35e50db4ad07a916ba2e83b2a2a5
Shaikhah Maasher <sh.maasher@gmail.com>
2018-11-19 23:41:16 +0300

    add menu overlay

--- js/app.js
+++ js/app.js
@@ -47,3 +47,12 @@ window.addEventListener('scroll', function () {
 });
 
 
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("nav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("nav").style.width = "0%";
+}
